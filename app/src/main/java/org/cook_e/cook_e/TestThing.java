65938f25cc6c2a2ce2e01b8bc48ccf506c6e9aa7
Sam Crow <samcrow@uw.edu>
2016-01-29 23:01:24 -0800

    Added meaningless functionality as a test

---
+++ app/src/main/java/org/cook_e/cook_e/TestThing.java
@@ -0,0 +1,16 @@
+package org.cook_e.cook_e;
+
+/**
+ * An example new feature
+ */
+public class TestThing {
+	public boolean isAThing() {
+		return true;
+	}
+	public boolean isATest() {
+		return true;
+	}
+	public boolean isEdible() {
+		return false;
+	}
+}
