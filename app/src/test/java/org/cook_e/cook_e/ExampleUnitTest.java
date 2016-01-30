85ac9868949868c0868d882a05d331af4a4223d3
Sam Crow <samcrow@uw.edu>
2016-01-29 16:40:05 -0800

    Added initial Android project files

---
+++ app/src/test/java/org/cook_e/cook_e/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package org.cook_e.cook_e;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+	@Test
+	public void addition_isCorrect() throws Exception {
+		assertEquals(4, 2 + 2);
+	}
+}
