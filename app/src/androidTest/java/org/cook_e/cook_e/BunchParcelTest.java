306d1639efbb5fc664349c6792c12e9771a5c74f
Sam Crow <samcrow@uw.edu>
2016-02-07 11:42:29 -0800

    Added parceling tests

---
+++ app/src/androidTest/java/org/cook_e/cook_e/BunchParcelTest.java
@@ -0,0 +1,98 @@
+package org.cook_e.cook_e;
+
+import android.os.Parcel;
+
+import org.cook_e.data.Bunch;
+import org.cook_e.data.Ingredient;
+import org.cook_e.data.Recipe;
+import org.cook_e.data.Step;
+import org.joda.time.Duration;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Tests parceling and unparceling of Bunches
+ */
+public class BunchParcelTest {
+
+	@Test
+	public void testNewArray() {
+		final int size = 32;
+		final Bunch[] array = Bunch.CREATOR.newArray(size);
+		assertNotNull(array);
+		assertEquals("Incorrect array length", size, array.length);
+	}
+
+	@Test
+	public void testParcelBasic() {
+		final Parcel parcel = Parcel.obtain();
+		final Bunch original = new Bunch("Meal 83", Collections.<Recipe>emptyList());
+
+		original.writeToParcel(parcel, 0);
+		parcel.setDataPosition(0);
+		final Bunch unparceled = Bunch.CREATOR.createFromParcel(parcel);
+		assertNotNull(unparceled);
+		assertEquals(original, unparceled);
+	}
+
+	@Test
+	public void testParcelOneRecipe() {
+		final Parcel parcel = Parcel.obtain();
+		final Ingredient scallops = new Ingredient("Scallops", 4, "");
+		final Step step = new Step(Collections.singletonList(scallops), "Poach", "Gently poach the scallops", Duration.standardMinutes(3));
+		final Recipe recipe = new Recipe("Recipe title", "Clamify Flumingaster", Collections.singletonList(step));
+		final Bunch original = new Bunch("Meal 83", Collections.singletonList(recipe));
+
+		original.writeToParcel(parcel, 0);
+		parcel.setDataPosition(0);
+		final Bunch unparceled = Bunch.CREATOR.createFromParcel(parcel);
+
+		assertNotNull(unparceled);
+		assertEquals(original, unparceled);
+	}
+
+	@Test
+	public void testParcelTwoRecipes() {
+		final Parcel parcel = Parcel.obtain();
+		final Ingredient scallops = new Ingredient("Scallops", 4, "");
+		final Ingredient butter = new Ingredient("Butter", 1, "kilogram");
+		final Step step1 = new Step(Collections.singletonList(scallops),
+				"Poach", "Gently poach the scallops", Duration.standardMinutes(2));
+		final Step step2 = new Step(Collections.singletonList(butter),
+				"Melt", "Melt the butter", Duration.standardMinutes(10));
+		final Recipe recipe1 = new Recipe("Recipe title", "Clamify Flumingaster", Arrays.asList(step1, step2));
+		final Recipe recipe2 = createRecipe2();
+		final Bunch original = new Bunch("Some meal", Arrays.asList(recipe1, recipe2));
+
+		original.writeToParcel(parcel, 0);
+		parcel.setDataPosition(0);
+		final Bunch unparceled = Bunch.CREATOR.createFromParcel(parcel);
+
+		assertNotNull(unparceled);
+		assertEquals(original, unparceled);
+	}
+
+	private static Recipe createRecipe2() {
+		final Ingredient whippedCream = new Ingredient("Whipped cream", 90, "milliliter");
+		final Ingredient lemonJuice = new Ingredient("Lemon juice", 140, "milliliter");
+
+		final List<Step> steps = new ArrayList<>();
+		steps.add(new Step(Collections.singletonList(lemonJuice), "Slice", "Slice the lemon juice", Duration.standardMinutes(1)));
+		steps.add(new Step(Collections.singletonList(whippedCream), "Saute", "Saute the whipped cream", Duration.standardMinutes(3)));
+		steps.add(new Step(Collections.<Ingredient>emptyList(), "Microwave", "Microwave the whipped cream", Duration.standardMinutes(2)));
+		steps.add(new Step(Collections.<Ingredient>emptyList(), "Heat", "Heat the whipped cream", Duration.standardMinutes(2)));
+		steps.add(new Step(Collections.<Ingredient>emptyList(), "Add", "Add the whipped cream", Duration.standardMinutes(1)));
+		steps.add(new Step(Collections.<Ingredient>emptyList(), "Stir", "Stir the lemon juice", Duration.standardMinutes(4)));
+		steps.add(new Step(Collections.<Ingredient>emptyList(), "Boil", "Bring the whipped cream to the boil", Duration.standardMinutes(10)));
+		steps.add(new Step(Collections.<Ingredient>emptyList(), "Order", "Order out", Duration.standardMinutes(30)));
+
+		return new Recipe("Battered whipped cream", "Random recipe generator", steps);
+	}
+}
--- app/src/androidTest/java/org/cook_e/cook_e/RecipeParcelTest.java
+++ app/src/androidTest/java/org/cook_e/cook_e/RecipeParcelTest.java
@@ -3,6 +3,7 @@ package org.cook_e.cook_e;
 import android.os.Parcel;
 
 import org.cook_e.data.Ingredient;
+import org.cook_e.data.Recipe;
 import org.cook_e.data.Step;
 import org.joda.time.Duration;
 import org.junit.Test;
@@ -15,14 +16,14 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
- * Tests parceling and unparceling of Steps
+ * Tests parceling and unparceling of Recipes
  */
-public class StepParcelTest {
+public class RecipeParcelTest {
 
 	@Test
 	public void testNewArray() {
 		final int size = 32;
-		final Step[] array = Step.CREATOR.newArray(size);
+		final Recipe[] array = Recipe.CREATOR.newArray(size);
 		assertNotNull(array);
 		assertEquals("Incorrect array length", size, array.length);
 	}
@@ -30,52 +31,49 @@ public class StepParcelTest {
 	@Test
 	public void testParcelBasic() {
 		final Parcel parcel = Parcel.obtain();
-		final Step original = new Step(new ArrayList<Ingredient>(), "Something",
-				"Do something with particular qualities", Duration.standardSeconds(30));
+		final Recipe original = new Recipe("Recipe title", "Clamify Flumingaster", new ArrayList<Step>());
 
 		original.writeToParcel(parcel, 0);
 		parcel.setDataPosition(0);
-		final Step unparceled = Step.CREATOR.createFromParcel(parcel);
+		final Recipe unparceled = Recipe.CREATOR.createFromParcel(parcel);
 		assertNotNull(unparceled);
 		assertEquals(original, unparceled);
 	}
 
 	@Test
-	public void testParcelOneIngredient() {
+	public void testParcelOneStep() {
 		final Parcel parcel = Parcel.obtain();
 		final Ingredient scallops = new Ingredient("Scallops", 4, "");
-		final Step original = new Step(Collections.singletonList(scallops), "Poach", "Gently poach the scallops", Duration.standardMinutes(3));
+		final Step step = new Step(Collections.singletonList(scallops), "Poach", "Gently poach the scallops", Duration.standardMinutes(3));
+		final Recipe original = new Recipe("Recipe title", "Clamify Flumingaster", Collections.singletonList(step));
 
 		original.writeToParcel(parcel, 0);
 		parcel.setDataPosition(0);
-		final Step unparceled = Step.CREATOR.createFromParcel(parcel);
+		final Recipe unparceled = Recipe.CREATOR.createFromParcel(parcel);
 
 		assertNotNull(unparceled);
 		assertEquals(original, unparceled);
 
-		final Ingredient unparceledIngredient = unparceled.getIngredients().get(0);
+		final Ingredient unparceledIngredient = unparceled.getSteps().get(0).getIngredients().get(0);
 		assertEquals(scallops, unparceledIngredient);
 	}
 
 	@Test
-	public void testParcelTwoIngredients() {
+	public void testParcelTwoSteps() {
 		final Parcel parcel = Parcel.obtain();
 		final Ingredient scallops = new Ingredient("Scallops", 4, "");
 		final Ingredient butter = new Ingredient("Butter", 1, "kilogram");
-		final Step original = new Step(Arrays.asList(scallops, butter),
-				"Poach", "Gently poach (fry?) the scallops in butter", Duration.standardMinutes(2));
+		final Step step1 = new Step(Collections.singletonList(scallops),
+				"Poach", "Gently poach the scallops", Duration.standardMinutes(2));
+		final Step step2 = new Step(Collections.singletonList(butter),
+				"Melt", "Melt the butter", Duration.standardMinutes(10));
+		final Recipe original = new Recipe("Recipe title", "Clamify Flumingaster", Arrays.asList(step1, step2));
 
 		original.writeToParcel(parcel, 0);
 		parcel.setDataPosition(0);
-		final Step unparceled = Step.CREATOR.createFromParcel(parcel);
+		final Recipe unparceled = Recipe.CREATOR.createFromParcel(parcel);
 
 		assertNotNull(unparceled);
 		assertEquals(original, unparceled);
-
-		final Ingredient unparceledScallops = unparceled.getIngredients().get(0);
-		assertEquals(scallops, unparceledScallops);
-
-		final Ingredient unparceledButter = unparceled.getIngredients().get(1);
-		assertEquals(butter, unparceledButter);
 	}
 }
