d7e4b6167bea398db43b391fa9c8d267bb72ae7d
Sam Crow <samcrow@uw.edu>
2016-02-19 13:30:07 -0800

    Started some changes

--- app/src/main/java/org/cook_e/cook_e/ui/RecipeList.java
+++ app/src/main/java/org/cook_e/cook_e/ui/RecipeList.java
@@ -19,7 +19,9 @@
 
 package org.cook_e.cook_e.ui;
 
+import android.app.AlertDialog;
 import android.content.Intent;
+import android.databinding.ObservableArrayList;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.support.design.widget.FloatingActionButton;
@@ -29,54 +31,34 @@ import android.view.ViewGroup;
 
 import org.cook_e.cook_e.CreateRecipe;
 import org.cook_e.cook_e.R;
+import org.cook_e.data.Recipe;
+import org.cook_e.data.StorageAccessor;
+
+import java.sql.SQLException;
+import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link RecipeList.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link RecipeList#newInstance} factory method to
- * create an instance of this fragment.
+ * A fragment that displays a list of recipes
  */
 public class RecipeList extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-
-    public RecipeList() {
-        // Required empty public constructor
-    }
 
     /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RecipeList.
+     * The recipes to display
      */
-    // TODO: Rename and change types and number of parameters
-    public static RecipeList newInstance(String param1, String param2) {
-        RecipeList fragment = new RecipeList();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private List<Recipe> mRecipes;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        final StorageAccessor accessor = new StorageAccessor(getContext());
+        try {
+            final ObservableArrayList<Recipe> recipes = new ObservableArrayList<>(accessor.loadAllRecipes());
+        } catch (SQLException e) {
+            new AlertDialog.Builder(getContext())
+                    .setTitle("Failed to load recipes")
+                    .setMessage(e.getLocalizedMessage())
+                    .show();
         }
     }
 
