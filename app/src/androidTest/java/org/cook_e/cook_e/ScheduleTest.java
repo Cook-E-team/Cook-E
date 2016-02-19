af3a0d5ebfb114371157cd152ce92aa62a760353
tsjacoby <tsjacoby@cs.washington.edu>
2016-02-18 21:03:10 -0800

    fixed problem where assertequals was ambigous from multiple imported version

--- app/src/androidTest/java/org/cook_e/cook_e/ScheduleTest.java
+++ app/src/androidTest/java/org/cook_e/cook_e/ScheduleTest.java
@@ -14,13 +14,10 @@ import org.joda.time.Duration;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static junit.framework.Assert.assertEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 public class ScheduleTest {
     final Ingredient scallops = new Ingredient("Scallops", 4, "");
