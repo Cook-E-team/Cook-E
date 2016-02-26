98fd30080070a7e3954f64939e78cc9df8cf5a29
pengxianzhe <pengxianzhe01@hotmail.com>
2016-02-25 20:23:43 -0800

    Add clear function for learner, insert comment for learner

--- app/src/main/java/org/cook_e/data/TimeLearner.java
+++ app/src/main/java/org/cook_e/data/TimeLearner.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2016 the Cook-E development team
+ *
+ *  This file is part of Cook-E.
+ *
+ *  Cook-E is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Cook-E is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Cook-E.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.cook_e.data;
 
 import android.support.annotation.NonNull;
@@ -79,6 +98,14 @@ public class TimeLearner {
             return new Duration(time);
         }
         return s.getTime().toDuration();
+    }
+
+    /**
+     * Clears all data stored in learner
+     */
+    public void clearLearner() {
+        weightList.clear();
+        // TODO: write empty list to storage
     }
 
     /**
